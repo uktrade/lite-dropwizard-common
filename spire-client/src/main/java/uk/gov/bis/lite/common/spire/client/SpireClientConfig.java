796f786b1c0747bc4e49a17e73c5226343a36542
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-08-18 14:09:52 +0100

    SpireClientConfig constructor accepts null read and connect timeouts

--- spire-client/src/main/java/uk/gov/bis/lite/common/spire/client/SpireClientConfig.java
+++ spire-client/src/main/java/uk/gov/bis/lite/common/spire/client/SpireClientConfig.java
@@ -1,6 +1,13 @@
 package uk.gov.bis.lite.common.spire.client;
 
+import java.util.Optional;
+
+import javax.annotation.Nullable;
+
 public class SpireClientConfig {
+
+  public static final int DEFAULT_CONNECT_TIMEOUT_MILLIS = 20000;
+  public static final int DEFAULT_READ_TIMEOUT_MILLIS = 60000;
 
   private final String username;
   private final String password;
@@ -11,16 +18,14 @@ public class SpireClientConfig {
   /**
    * Config for Spire Soap connection
    *
-   * @param username spire connection username
-   * @param password spire connection password
-   * @param url      spire connection base url
+   * @param username              spire connection username
+   * @param password              spire connection password
+   * @param url                   spire connection base url
+   *
+   * @see #SpireClientConfig(String username, String password, String url, Integer connectTimeoutMillis, Integer readTimeoutMillis)
    */
   public SpireClientConfig(String username, String password, String url) {
-    this.username = username;
-    this.password = password;
-    this.url = url;
-    this.connectTimeoutMillis = 20000;
-    this.readTimeoutMillis = 60000;
+    this(username, password, url, null, null);
   }
 
   /**
@@ -29,15 +34,15 @@ public class SpireClientConfig {
    * @param username              spire connection username
    * @param password              spire connection password
    * @param url                   spire connection base url
-   * @param connectTimeoutMillis  connection timeout in milliseconds
-   * @param readTimeoutMillis     read timeout in milliseconds
+   * @param connectTimeoutMillis  connection timeout in milliseconds, defaults to {@value #DEFAULT_CONNECT_TIMEOUT_MILLIS} when null
+   * @param readTimeoutMillis     read timeout in milliseconds, defaults to {@value #DEFAULT_READ_TIMEOUT_MILLIS} when null
    */
-  public SpireClientConfig(String username, String password, String url, int connectTimeoutMillis, int readTimeoutMillis) {
+  public SpireClientConfig(String username, String password, String url, @Nullable Integer connectTimeoutMillis, @Nullable Integer readTimeoutMillis) {
     this.username = username;
     this.password = password;
     this.url = url;
-    this.connectTimeoutMillis = connectTimeoutMillis;
-    this.readTimeoutMillis = readTimeoutMillis;
+    this.connectTimeoutMillis = Optional.ofNullable(connectTimeoutMillis).orElse(DEFAULT_CONNECT_TIMEOUT_MILLIS);
+    this.readTimeoutMillis = Optional.ofNullable(readTimeoutMillis).orElse(DEFAULT_READ_TIMEOUT_MILLIS);
   }
 
   String getUsername() {
