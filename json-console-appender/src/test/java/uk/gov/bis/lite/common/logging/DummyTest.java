e8181f9955508a1f05f3079b5ae8d68a0074e5a1
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-08-18 10:20:23 +0100

    Added dummy tests

---
+++ json-console-appender/src/test/java/uk/gov/bis/lite/common/logging/DummyTest.java
@@ -0,0 +1,13 @@
+package uk.gov.bis.lite.common.logging;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+  @Test
+  public void dummyTest() throws Exception {
+    assertEquals(1,1);
+  }
+}
---
+++ readiness-metric/src/test/java/uk/gov/bis/lite/common/metrics/readiness/DummyTest.java
@@ -0,0 +1,13 @@
+package uk.gov.bis.lite.common.metrics.readiness;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+  @Test
+  public void dummyTest() throws Exception {
+    assertEquals(1,1);
+  }
+}
