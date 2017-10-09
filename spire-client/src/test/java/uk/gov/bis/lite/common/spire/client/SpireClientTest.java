5aa3fc683cd5bcf67fc5a3b43e2deac6c6d63e99
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-10-09 14:59:31 +0100

    Use dynamic port for WireMock stubbing in SpireClientTest

--- spire-client/src/test/java/uk/gov/bis/lite/common/spire/client/SpireClientTest.java
+++ spire-client/src/test/java/uk/gov/bis/lite/common/spire/client/SpireClientTest.java
@@ -1,45 +1,31 @@
 package uk.gov.bis.lite.common.spire.client;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static io.dropwizard.testing.FixtureHelpers.fixture;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import uk.gov.bis.lite.common.spire.client.exception.SpireClientException;
 import uk.gov.bis.lite.common.spire.client.parser.ReferenceParser;
-import uk.gov.bis.lite.common.spire.client.parser.SpireParser;
 
 public class SpireClientTest {
 
-  private SpireClient<String> client;
+  @Rule
+  public WireMockRule wireMockRule = new WireMockRule(options().dynamicPort());
 
-  @ClassRule
-  public static WireMockRule wireMockRule = new WireMockRule(8089);
-
-  public SpireClientTest() {
-    SpireParser<String> parser = new ReferenceParser("ELEMENT");
-    SpireClientConfig clientConfig = new SpireClientConfig("username", "password", "http://localhost:8089/");
-    SpireRequestConfig requestConfig = new SpireRequestConfig("NAMESPACE", "CHILD", false);
-    client = new SpireClient<>(parser, clientConfig, requestConfig);
-  }
-
-  @BeforeClass
-  public static void before() throws Exception {
-    wireMockRule.start();
-  }
-
-  @AfterClass
-  public static void after() throws Exception {
-    wireMockRule.stop();
+  @Before
+  public void before() throws Exception {
+    configureFor(wireMockRule.port());
   }
 
   @Test
@@ -55,6 +41,12 @@ public class SpireClientTest {
         )
     );
 
+    String spireUrl = "http://localhost:" + wireMockRule.port() + "/";
+    SpireClient<String> client =  new SpireClient<>(
+        new ReferenceParser("ELEMENT"),
+        new SpireClientConfig("username", "password", spireUrl),
+        new SpireRequestConfig("NAMESPACE", "CHILD", false));
+
     SpireRequest request = client.createRequest();
     String response = client.sendRequest(request);
 
@@ -64,11 +56,6 @@ public class SpireClientTest {
 
   @Test
   public void testSimpleValidRequestUsingSpirePrefix() {
-    SpireClient<String> spireClient = new SpireClient<>(
-        new ReferenceParser("ELEMENT"),
-        new SpireClientConfig("username", "password", "http://localhost:8089/"),
-        new SpireRequestConfig("NAMESPACE", "CHILD", true));
-
     stubFor(post(urlEqualTo("/NAMESPACE"))
         .withHeader("Authorization", equalTo("Basic dXNlcm5hbWU6cGFzc3dvcmQ="))
         .withHeader("Content-Type", equalTo("text/xml; charset=UTF-8"))
@@ -80,8 +67,14 @@ public class SpireClientTest {
         )
     );
 
-    SpireRequest request = spireClient.createRequest();
-    String response = spireClient.sendRequest(request);
+    String spireUrl = "http://localhost:" + wireMockRule.port() + "/";
+    SpireClient<String> client =  new SpireClient<>(
+        new ReferenceParser("ELEMENT"),
+        new SpireClientConfig("username", "password", spireUrl),
+        new SpireRequestConfig("NAMESPACE", "CHILD", true));
+
+    SpireRequest request = client.createRequest();
+    String response = client.sendRequest(request);
 
     // Verify response
     assertThat(response).isEqualTo("TEXT");
@@ -93,6 +86,13 @@ public class SpireClientTest {
         .willReturn(aResponse()
             .withStatus(500))
     );
+
+    String spireUrl = "http://localhost:" + wireMockRule.port() + "/";
+    SpireClient<String> client =  new SpireClient<>(
+        new ReferenceParser("ELEMENT"),
+        new SpireClientConfig("username", "password", spireUrl),
+        new SpireRequestConfig("NAMESPACE", "CHILD", false));
+
     SpireRequest request = client.createRequest();
     assertThatThrownBy(() -> client.sendRequest(request))
         .isExactlyInstanceOf(SpireClientException.class)
@@ -101,11 +101,6 @@ public class SpireClientTest {
 
   @Test
   public void testUrlMissingTrailingSlash() {
-    SpireParser<String> parser = new ReferenceParser("ELEMENT");
-    // Note, url missing trailing slash
-    SpireClientConfig clientConfig = new SpireClientConfig("username", "password", "http://localhost:8089/some-path");
-    SpireRequestConfig requestConfig = new SpireRequestConfig("NAMESPACE", "CHILD", false);
-    SpireClient<String> client = new SpireClient<>(parser, clientConfig, requestConfig);
     stubFor(post(urlEqualTo("/some-path/NAMESPACE"))
         .willReturn(aResponse()
             .withStatus(200)
@@ -113,6 +108,14 @@ public class SpireClientTest {
             .withBodyFile("simple.xml")
         )
     );
+
+    // Note, url missing trailing slash
+    String spireUrl = "http://localhost:" + wireMockRule.port() + "/some-path";
+    SpireClient<String> client =  new SpireClient<>(
+        new ReferenceParser("ELEMENT"),
+        new SpireClientConfig("username", "password", spireUrl),
+        new SpireRequestConfig("NAMESPACE", "CHILD", false));
+
     SpireRequest request = client.createRequest();
     String response = client.sendRequest(request);
     assertThat(response).isEqualTo("TEXT");
